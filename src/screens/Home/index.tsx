1ced8ba2194015bf53c89b14350b1031d4715a80
Isaque Chaves <isaque.ramosch@gmail.com>
2024-06-22 13:18:46 -0300

    feat: add Home page

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, TextInput } from "react-native";
+import { styles } from "./styles";
+
+export function Home(){
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText}>O que você quer assistir hoje?</Text>
+      <View style={styles.containerInput}>
+        <TextInput placeholder="Buscar" />
+      </View>
+    </View>
+  )
+}
